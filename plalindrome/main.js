7b9baa9917a24955320ce3130d3f45ef5e7a3312
Calcifer <calciferzh@outlook.com>
2017-12-30 21:33:43 +0800

    add sample js

---
+++ plalindrome/main.js
@@ -0,0 +1,17 @@
+var string1 = 'this is some fxxking string';
+var string2 = 'this is a si siht';
+var len1 = 27;
+var len2 = 17;
+var i = 0;
+var flag = 1;
+while (i < len2) {
+  if (string2[i] != string2[len2 - i]) {
+    flag = 0;
+  }
+}
+
+if (flag == 0) {
+  print('Yes');
+} else {
+  print('No');
+}
